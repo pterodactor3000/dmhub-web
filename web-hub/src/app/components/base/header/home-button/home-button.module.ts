c42374ae2c602277728dee42f21ca6553a6a0acb
pterodactor3000 <mikolaj.grygorcewicz@gmail.com>
2022-08-15 14:18:40 +0200

    Fallback to simple arrow - for now

--- web-hub/src/app/components/base/header/home-button/home-button.module.ts
+++ web-hub/src/app/components/base/header/home-button/home-button.module.ts
@@ -1,10 +1,11 @@
+import { WbHbModule } from './../../../../wb-hb.module';
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { HomeButtonComponent } from './home-button.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [HomeButtonComponent],
-  imports: [CommonModule],
+  imports: [WbHbModule, RouterModule],
   exports: [HomeButtonComponent],
 })
 export class HomeButtonModule {}
