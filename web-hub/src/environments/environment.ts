9a467c54c32743ca1aefd3efffd9d6ef3fc84fc5
pterodactor3000 <mikolaj.grygorcewicz@gmail.com>
2022-08-21 18:33:21 +0200

    Added login and logout with firebase

--- web-hub/src/environments/environment.ts
+++ web-hub/src/environments/environment.ts
@@ -7,6 +7,16 @@ export const environment = {
   baseUrl: 'https://us-central1-dmtool-cad62.cloudfunctions.net/',
   title: 'WebHub',
   description: 'Web catalog of DMHub resources',
+  firebase: {
+    apiKey: 'AIzaSyCp2iE9wj-sKjUy2w4-ndjEediUGvB5YwY',
+    // authDomain: '<your-project-authdomain>',
+    // databaseURL: '<your-database-URL>',
+    // projectId: '<your-project-id>',
+    // storageBucket: '<your-storage-bucket>',
+    // messagingSenderId: '<your-messaging-sender-id>',
+    // appId: '<your-app-id>',
+    // measurementId: '<your-measurement-id>',
+  },
 };
 
 /*
