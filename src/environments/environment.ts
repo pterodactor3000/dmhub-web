5f813e669b66b452a86ae31a0565302c6fc6a54f
pterodactor3000 <mikolaj.grygorcewicz@gmail.com>
2022-11-06 14:27:43 +0100

    chore(app): :rocket: moved sensitive data to github sercrets and locally to .env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,23 +1,10 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
   production: false,
   baseUrl: 'https://us-central1-dmtool-cad62.cloudfunctions.net/',
   title: 'WebHub',
   description: 'Web catalog of DMHub resources',
   firebase: {
-    apiKey: 'AIzaSyCp2iE9wj-sKjUy2w4-ndjEediUGvB5YwY',
+    apiKey: '',
   },
-  refreshRate: 1000 * 60 * 60,
+  refreshRate: 1000 * 10,
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
