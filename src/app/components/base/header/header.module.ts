bc900fa9691c37d74623fdf6238be0da379d4fca
pterodactor3000 <mikolaj.grygorcewicz@gmail.com>
2022-12-21 13:27:23 +0100

    feat(app): :lipstick: some work on styles and imports

--- src/app/components/base/header/header.module.ts
+++ src/app/components/base/header/header.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { HeaderComponent } from './header.component';
 import { WbHbModule } from 'src/app/wb-hb.module';
-import { HomeButtonModule } from './home-button/home-button.module';
 import { ButtonModule } from '../button/button.module';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [HeaderComponent],
-  imports: [WbHbModule, HomeButtonModule, ButtonModule],
+  imports: [WbHbModule, ButtonModule, RouterModule],
   exports: [HeaderComponent],
 })
 export class HeaderModule {}
