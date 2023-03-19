004ebd0236fc91c5ef07316eac6889b3ca2caa34
pterodactor3000 <mikolaj.grygorcewicz@gmail.com>
2023-03-20 00:44:25 +0100

    chore(app): update packages and migrate to yarn

---
+++ src/environments/environment.onprem.ts
@@ -0,0 +1,11 @@
+
+export const environment = {
+  production: false,
+  baseUrl: 'https://us-central1-dmtool-cad62.cloudfunctions.net/',
+  title: 'WebHub',
+  description: 'Web catalog of DMHub resources',
+  firebase: {
+    apiKey: 'AIzaSyCp2iE9wj-sKjUy2w4-ndjEediUGvB5YwY',
+  },
+  refreshRate: 10000,
+};
